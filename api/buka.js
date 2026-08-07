812db798c5cf85aa750e6823485434eadf4d0c9a
mazuwanmustaffa <mazuwan.mustaffa@kkkla.edu.my>
2026-08-07 16:18:38 +0800

    Update buka.js

--- api/buka.js
+++ api/buka.js
@@ -1,12 +1,13 @@
 const { TuyaContext } = require('@tuya/tuya-connector-nodejs');
 
 const tuya = new TuyaContext({
-  baseUrl: 'https://openapi-sg.iotbing.com', // Endpoint Rasmi Singapore Data Center
+  baseUrl: 'https://openapi.tuyacn.com', // URL standard bagi Singapore Data Center
   accessKey: '5apwu48xt5pexrxh5sf',
   secretKey: 'eeb83dbad3624ec19b74a72b989d6f8f',
 });
 
 module.exports = async (req, res) => {
+  // Tetapan CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -18,6 +19,7 @@ module.exports = async (req, res) => {
   try {
     const deviceId = 'a349e338f1fd700cc8u0xo';
 
+    // Menggunakan fungsi request rasmi Tuya
     const response = await tuya.request({
       path: `/v1.0/devices/${deviceId}/commands`,
       method: 'POST',
