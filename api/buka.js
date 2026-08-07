cfb41d8fd9e9666f471268bed96533c192958f82
mazuwanmustaffa <mazuwan.mustaffa@kkkla.edu.my>
2026-08-07 16:13:11 +0800

    Update buka.js

--- api/buka.js
+++ api/buka.js
@@ -1,11 +1,4 @@
-const { TuyaContext } = require('@tuya/tuya-connector-nodejs');
-
-const tuya = new TuyaContext({
-  baseUrl: 'https://openapi.tuyacn.com',
-  accessKey: '5apwu48xt5pexrxh5sf', // Masukkan kedua-duanya untuk elak header undefined
-  accessId: '5apwu48xt5pexrxh5sf',  
-  secretKey: 'eeb83dbad3624ec19b74a72b989d6f8f',
-});
+const crypto = require('crypto');
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,24 +9,66 @@ module.exports = async (req, res) => {
     return res.status(200).end();
   }
 
-  try {
-    const deviceId = 'a349e338f1fd700cc8u0xo';
+  const clientId = '5apwu48xt5pexrxh5sf';
+  const secret = 'eeb83dbad3624ec19b74a72b989d6f8f';
+  const deviceId = 'a349e338f1fd700cc8u0xo';
+  const baseUrl = 'https://openapi.tuyacn.com';
 
-    const response = await tuya.request({
-      path: `/v1.0/devices/${deviceId}/commands`,
-      method: 'POST',
-      body: {
-        commands: [
-          {
-            code: 'switch_1',
-            value: true,
-          },
-        ],
-      },
+  try {
+    // 1. Ambil Token
+    const t = Date.now().toString();
+    const tokenUrl = '/v1.0/token?grant_type=1';
+    
+    const stringToSign = `GET\ne3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855\n\n${tokenUrl}`;
+    const signStr = clientId + t + stringToSign;
+    const sign = crypto.createHmac('sha256', secret).update(signStr).digest('hex').toUpperCase();
+
+    const tokenRes = await fetch(baseUrl + tokenUrl, {
+      method: 'GET',
+      headers: {
+        'client_id': clientId,
+        'sign': sign,
+        't': t,
+        'sign_method': 'HMAC-SHA256'
+      }
     });
 
-    return res.status(200).json(response);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
+    const tokenData = await tokenRes.json();
+
+    if (!tokenData.success) {
+      return res.status(400).json(tokenData);
+    }
+
+    const accessToken = tokenData.result.access_token;
+
+    // 2. Hantar Perintah Buka Pintu
+    const cmdUrl = `/v1.0/devices/${deviceId}/commands`;
+    const bodyData = { commands: [{ code: 'switch_1', value: true }] };
+    const bodyStr = JSON.stringify(bodyData);
+    
+    const t2 = Date.now().toString();
+    const bodyHash = crypto.createHash('sha256').update(bodyStr).digest('hex');
+    const stringToSign2 = `POST\n${bodyHash}\n\n${cmdUrl}`;
+    const signStr2 = clientId + accessToken + t2 + stringToSign2;
+    const sign2 = crypto.createHmac('sha256', secret).update(signStr2).digest('hex').toUpperCase();
+
+    const cmdRes = await fetch(baseUrl + cmdUrl, {
+      method: 'POST',
+      headers: {
+        'client_id': clientId,
+        'access_token': accessToken,
+        'sign': sign2,
+        't': t2,
+        'sign_method': 'HMAC-SHA256',
+        'Content-Type': 'application/json'
+      },
+      body: bodyStr
+    });
+
+    const cmdData = await cmdRes.json();
+    return res.status(200).json(cmdData);
+
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
   }
 };
