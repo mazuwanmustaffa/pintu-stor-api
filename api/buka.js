a22f618c021ae4e4be99c6d59846c3f530924717
mazuwanmustaffa <mazuwan.mustaffa@kkkla.edu.my>
2026-08-07 15:38:58 +0800

    Update buka.js

--- api/buka.js
+++ api/buka.js
@@ -1,32 +1,36 @@
 const { TuyaContext } = require('@tuya/tuya-connector-nodejs');
 
 const tuya = new TuyaContext({
-  baseUrl: 'https://openapi.tuyaus.com', // Asia-Pacific Data Center
-  accessKey: '5apwu48xt55pexrxh5sf',
+  baseUrl: 'https://openapi.tuyacn.com',
+  accessKey: '5apwu48xt5pexrxh5sf',
   secretKey: 'eeb83dbad3624ec19b74a72b989d6f8f',
 });
 
 module.exports = async (req, res) => {
-  // Benarkan akses CORS dari web anda
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Tetapan CORS untuk benarkan akses dari web inventori anda
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
+    return res.status(200).end();
   }
 
   try {
+    const deviceId = 'a349e338f1fd700cc8u0xo';
+
+    // Perintah untuk membuka kunci pintu
     const response = await tuya.request({
-      path: '/v1.0/devices/a349e338f1fd700cc8u0xo/commands',
+      path: `/v1.0/devices/${deviceId}/commands`,
       method: 'POST',
       body: {
         commands: [
-          { code: 'switch_1', value: true },
-          { code: 'switch', value: true }
-        ]
-      }
+          {
+            code: 'switch_1',
+            value: true,
+          },
+        ],
+      },
     });
 
     return res.status(200).json(response);
