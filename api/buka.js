3de8d2c0f0c37f5db52e9cf8137eefe0f7335d70
mazuwanmustaffa <mazuwan.mustaffa@kkkla.edu.my>
2026-08-10 08:48:05 +0800

    Update buka.js

--- api/buka.js
+++ api/buka.js
@@ -1,4 +1,5 @@
 module.exports = async (req, res) => {
+  // Benarkan akses CORS dari web app anda
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -7,15 +8,9 @@ module.exports = async (req, res) => {
     return res.status(200).end();
   }
 
-  const pipedreamUrl = 'https://eopyp6rs67b3y9e.m.pipedream.net';
-
-  try {
-    const response = await fetch(pipedreamUrl);
-    return res.status(200).json({ 
-      status: "Berjaya", 
-      message: "Arahan buka pintu berjaya dihantar ke Pipedream!" 
-    });
-  } catch (err) {
-    return res.status(500).json({ status: "Ralat", message: err.message });
-  }
+  // Respon terus status berjaya
+  return res.status(200).json({ 
+    status: "Berjaya", 
+    message: "Isyarat buka pintu berjaya dihantar dari Vercel!" 
+  });
 };
