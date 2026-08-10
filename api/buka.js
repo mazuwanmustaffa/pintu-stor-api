03bcd0718fffbd7c8058955afc28a66c37ca13a1
mazuwanmustaffa <mazuwan.mustaffa@kkkla.edu.my>
2026-08-10 09:22:10 +0800

    Update buka.js

--- api/buka.js
+++ api/buka.js
@@ -5,7 +5,7 @@ const https = require('https');
 const CLIENT_ID = '5apwu48xt5spexaxh5sf';
 const CLIENT_SECRET = 'eeb83dbad3624ec19b74a72b989d6f8f';
 const DEVICE_ID = 'a349e338f1fd700cc8u0xo';
-const TUYA_HOST = 'openapi.tuyasg.com'; // Host Singapore yang betul
+const TUYA_HOST = 'openapi.tuyasg.com'; // Endpoint Singapore Tuya yang tepat
 
 function calcSign(clientId, secret, t, accessToken = '', nonce = '', stringToSign = '') {
   const str = clientId + accessToken + t + nonce + stringToSign;
