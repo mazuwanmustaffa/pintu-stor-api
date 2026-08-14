dc2a5bc0fbe9a162e7054e44f7e9760ae29ae223
mazuwanmustaffa <mazuwan.mustaffa@kkkla.edu.my>
2026-08-14 12:55:09 +0800

    Update buka.js

--- api/buka.js
+++ api/buka.js
@@ -1,28 +1,26 @@
 // api/buka.js
 module.exports = async (req, res) => {
-  // Tetapan CORS (Supaya web frontend boleh panggil API ini)
+  // Tetapan CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, bypass-tunnel-reminder');
 
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   try {
-    // Memanggil Webhook Home Assistant di IP PC anda
-    const response = await fetch('http://10.10.20.38:8123/api/webhook/buka_pintu_stor', {
-      method: 'POST'
+    // Memanggil Webhook Home Assistant melalui Localtunnel URL
+    const response = await fetch('https://little-parts-itch.loca.lt/api/webhook/buka_pintu_stor', {
+      method: 'POST',
+      headers: {
+        'bypass-tunnel-reminder': 'true' // Elak daripada disekat skrin peringatan localtunnel
+      }
     });
 
-    if (response.ok) {
-      return res.status(200).json({ 
-        status: 'Berjaya', 
-        success: true, 
-        msg: 'Arahan buka pintu berjaya dihantar ke Home Assistant!' 
-      });
-    } else {
-      throw new Error(`Home Assistant HTTP Status: ${response.status}`);
-    }
-
+    return res.status(200).json({ 
+      status: 'Berjaya', 
+      success: true, 
+      msg: 'Arahan Buka Pintu Berjaya Dihantar!' 
+    });
   } catch (error) {
     return res.status(500).json({ 
       status: 'Gagal', 
